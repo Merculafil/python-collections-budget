bf079a289946467ed3c6a0bed44a215481ad8eb8
Alecsander Franzén <alecsander.franzen@sigmatechnology.se>
2022-09-23 12:01:28 +0200

    Module 2 Complete

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -3,22 +3,22 @@ from . import Expense
 
 class BudgetList:
     def __init__(self, budget):
-        self._budget = budget
-        self._sum_expenses = 0
-        self._expenses = []
-        self._sum_overages = 0
-        self._overages = []
+        self.budget = budget
+        self.sum_expenses = 0
+        self.expenses = []
+        self.sum_overages = 0
+        self.overages = []
 
     def append(self, item):
-        if self._sum_expenses + item < self._budget:
-            self._expenses.append(item)
-            self._sum_expenses += item
+        if self.sum_expenses + item < self.budget:
+            self.expenses.append(item)
+            self.sum_expenses += item
         else:
-            self._overages.append(item)
-            self._sum_overages += item
+            self.overages.append(item)
+            self.sum_overages += item
 
     def __len__(self):
-        return len(self._expenses) + len(self._overages)
+        return len(self.expenses) + len(self.overages)
 
 def main():
     myBudgetList = BudgetList(1200)
@@ -28,7 +28,7 @@ def main():
     for expense in expenses.list:
         myBudgetList.append(expense.amount)
 
-    print('The count of all expenses:', str(len(myBudgetList)))
+    print('The count of all expenses: ' + str(len(myBudgetList)))
 
 if __name__ == "__main__":
     main()
